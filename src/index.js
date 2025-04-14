a7cefacb71e79ddf0feed60bb5276bae438f96a8
Chronic-W-F <vandergriff4004@comcast.net>
2025-04-14 00:17:43 -0500

    Delete src/index.js

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import NutrientCalculator from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<NutrientCalculator />);
