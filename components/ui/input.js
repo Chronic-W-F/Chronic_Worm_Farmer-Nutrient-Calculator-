23ead9ab60579c3f6427b22507c7c40a3587b569
Chronic-W-F <vandergriff4004@comcast.net>
2025-04-15 02:18:17 -0500

    Create input.js

---
+++ components/ui/input.js
@@ -0,0 +1,8 @@
+export function Input(props) {
+  return (
+    <input
+      className="w-full border border-gray-300 rounded-md px-3 py-2 text-sm shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+      {...props}
+    />
+  );
+}
