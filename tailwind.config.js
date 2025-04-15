8d32aca47fdaa9e0698ffd49fc76fbf97a92d71d
Chronic-W-F <vandergriff4004@comcast.net>
2025-04-15 04:42:59 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+],
   safelist: [
     {
       pattern: /bg-(green|yellow|orange|red|blue)-\d{3}/,
